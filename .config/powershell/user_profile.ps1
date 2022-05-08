cd3a9e2babbac146d18542b308d91c7cb68f0c25
Arturo Avila <69410272+ADPenrose@users.noreply.github.com>
2022-05-07 23:12:32 -0500

    Added user profile and custom theme files for powershell.

---
+++ .config/powershell/user_profile.ps1
@@ -0,0 +1,15 @@
+# set PowerShell to UTF-8
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
+#Prompt
+Import-Module posh-git
+$omp_config = Join-Path $PSScriptRoot ".\takuya.omp.json"
+oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+Import-Module -Name Terminal-Icons
+
+#PSReadline
+Set-PSReadLineOption -PredictionSource History
+
+# Alias
+Set-Alias vim nvim
+Set-Alias touch New-Item
